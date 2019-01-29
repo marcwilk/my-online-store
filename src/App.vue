32f440997d79189325f7655a1e2cd8a063cc549c
Marc <marc.wilk@colorado.edu>
2019-01-29 11:34:03 -0700

    functioning without database

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <template>
   <div id="app" class="container my-5">
-    <h1>My online store</h1>
+    <div class="row mb-3">
+      <div class="col-md-9">
+        <h1>My online store</h1>
+      </div>
+      <div class="col-md-3">
+        <shoppingCart />
+      </div>
+    </div>
+    <div class="row">
+      <Item
+        v-for="item in forSale"
+        :key="item.invId"
+        :invId="item.invId"
+        :name="item.name"
+        :image="item.image"
+        :price="item.price"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Item from './Item.vue';
+import ShoppingCart from './ShoppingCart.vue';
+
 export default {
   name: 'app',
-  data () {
-    return {
-
-    }
-  }
-}
+  computed: {
+    forSale() { return this.$store.getters.forSale; },
+    inCart() { return this.$store.getters.inCart; },
+  },
+  components: {
+    Item,
+    ShoppingCart,
+  },
+};
 </script>
 
 <style>
