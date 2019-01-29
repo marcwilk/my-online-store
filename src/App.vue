035ad2a07429039e02995680c350e2db86facc1b
Marc <marc.wilk@colorado.edu>
2019-01-29 09:58:03 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="container my-5">
+    <h1>My online store</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
